df4aca648691cb697ab06300ba3b7fd83e40defc
chanellesf <chfriend@calpoly.edu>
2024-11-12 13:46:47 -0800

    completed lab 7 11/11/24

--- command_line.py
+++ command_line.py
@@ -1,1 +1,16 @@
+import sys
+from types import NoneType
 
+import convert
+
+# Converts each command-line argument in sys.argv into a float
+total = 0.0
+
+for i in range(1, len(sys.argv)):
+    if (type(convert.str_to_float(sys.argv[i])) != NoneType):
+        total = total + convert.str_to_float(sys.argv[i])
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    print("The sum is: ", total)
